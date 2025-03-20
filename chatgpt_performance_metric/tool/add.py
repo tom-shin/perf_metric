b8577c767fa9c5ddf0cb18e402c1ebbe1436f6a7
tom-shin <tom.shin@thundersoft.com>
2025-03-20 23:28:11 +0900

    added
    debug scenario added.

--- chatgpt_performance_metric/tool/add.py
+++ chatgpt_performance_metric/tool/add.py
@@ -1,18 +1,36 @@
 import json
 from collections import OrderedDict
 
-f = rf"C:\Work\tom\python_project\Testset_Generation_Evaluation\perf_metric\chatgpt_performance_metric\TestSet_Result.json"
+f = rf"C:\Users\ADMIN\Documents\tom\perf_metric\chatgpt_performance_metric\Evaluation_Release_Result\20241018\ragas_evaluation_result_241018.json"
 encoding = "utf-8"
 
 # 파일 열기 및 데이터 읽기
 with open(f, "r", encoding=encoding) as file:
     json_data = json.load(file, object_pairs_hook=OrderedDict)
 
+new_data = {
+    "user_input": "",
+    "response": "",
+    "file": "",
+    "reference_contexts": "",
+    "synthesizer_name": "",
+    "retrieved_contexts": "",
+    "chatbot_response": "",
+    "date_time": "",
+    "chatbot_server": "",
+    "reference": "",
+    "user_comment": "",
+    "user": ""
+  }
+
 # 데이터 수정
+new_tc = []
 for data in json_data:
-    data["user_comment"] = ""  # 'user_comment' 추가
-    data["user"] = ""  # 'user' 추가
+    new_data["user_input"] = data["question"]
+
+    new_tc.append(new_data)
 
 # 수정된 데이터를 다시 파일에 저장
+f = rf"C:\Users\ADMIN\Documents\tom\perf_metric\chatgpt_performance_metric\Evaluation_Release_Result\20241018\old_ragas_evaluation_result_241018.json"
 with open(f, 'w', encoding='utf-8') as file:
-    json.dump(json_data, file, ensure_ascii=False, indent=2)
+    json.dump(new_tc, file, ensure_ascii=False, indent=2)
--- chatgpt_performance_metric/tool/randomshuffle.py
+++ chatgpt_performance_metric/tool/randomshuffle.py
@@ -1,20 +1,21 @@
 import json
 import random
 
-# JSON 파일 읽기
-path_ = rf"C:\Work\tom\python_project\Testset_Generation_Evaluation\perf_metric\chatgpt_performance_metric\shuffled_file.json"
-path = path_.replace("\\", "/")
+for i in range(20):
+    # JSON 파일 읽기
+    path_ = rf"C:\Users\ADMIN\Documents\tom\perf_metric\chatgpt_performance_metric\shuffled_file.json"
+    path = path_.replace("\\", "/")
 
-with open(path, 'r', encoding='utf-8') as f:
-    data = json.load(f)  # data는 list[dict] 형태
+    with open(path, 'r', encoding='utf-8') as f:
+        data = json.load(f)  # data는 list[dict] 형태
 
-# 리스트 섞기
-random.shuffle(data)
+    # 리스트 섞기
+    random.shuffle(data)
 
-# 섞인 결과 확인
-for cnt, item in enumerate(data):
-    print(cnt, item)
+    # 섞인 결과 확인
+    for cnt, item in enumerate(data):
+        print(cnt, item)
 
-# (선택) 다시 파일로 저장하고 싶다면:
-with open('shuffled_file.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+    # (선택) 다시 파일로 저장하고 싶다면:
+    with open(rf'shuffled_file_{i}.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
